a414ca06112c4f6a53b1ba45068a8a3dc6bb3c38
doderick <doderick@outlook.com>
2019-07-06 15:11:40 +0800

    优化前端视图，移植laravel-mix

--- application/common.php
+++ application/common.php
@@ -10,3 +10,34 @@
 // +----------------------------------------------------------------------
 
 // 应用公共文件
+
+if (!function_exists('mix')) {
+    /**
+     * 获取hash后的资源路径
+     * 参考laravel5.5
+     *
+     * @param string $path
+     * @param string $manifestDirectory
+     * @return string
+     */
+    function mix($path, $manifestDirectory = '')
+    {
+        static $manifests = [];
+
+        if (substr($path, 0, 1) !== '/') {
+            $path = "/{$path}";
+        }
+        if ($manifestDirectory && substr($manifestDirectory, 0 , 1) !== '/') {
+            $manifestDirectory = "/{$manifestDirectory}";
+        }
+
+        $publicPath = Env::get('root_path') . 'public';
+        $manifestPath = $publicPath . $manifestDirectory . '/mix-manifest.json';
+        if (!isset($manifests[$manifestPath])) {
+            $manifests[$manifestPath] = json_decode(file_get_contents($manifestPath), true);
+        }
+
+        $manifest = $manifests[$manifestPath];
+        return Url::build($manifestDirectory . $manifest[$path], '', false);
+    }
+}
