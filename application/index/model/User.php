7b114e560cc7e22387379b00c1e60857773454d6
doderick <doderick@outlook.com>
2019-12-26 00:00:08 +0800

    粉丝数据表

--- application/index/model/User.php
+++ application/index/model/User.php
@@ -32,4 +32,57 @@ class User extends Model
         return $this->statuses()
                     ->order('create_time', 'desc');
     }
+
+    // 关联粉丝
+    public function followers()
+    {
+        return $this->belongsToMany('User', 'followers', 'user_id', 'follower_id');
+    }
+
+    // 关联关注的人
+    public function followings()
+    {
+        return $this->belongsToMany('User', 'followers', 'follower_id', 'user_id');
+    }
+
+    /**
+     * 用户进行关注的方法
+     *
+     * @param array|integer $user_ids 被关注人的id
+     * @return void
+     */
+    public function follow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+
+        $this->followings()->sync($user_ids);
+    }
+
+    /**
+     * 用户取消关注的方法
+     *
+     * @param array|integer $user_ids 取消关注的用户的id
+     * @return void
+     */
+    public function unfollow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+
+        $this->followings()->detach($user_ids);
+    }
+
+    /**
+     * 查询是否存在关联
+     *
+     * @param integer $user_id 需要查询的id
+     * @return boolean
+     */
+    public function isFollowing($user_id)
+    {
+        return $this->followings()->attached($user_id);
+    }
 }
---
+++ database/migrations/20191225221847_create_followers_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use think\migration\Migrator;
+use think\migration\db\Column;
+
+class CreateFollowersTable extends Migrator
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $table = $this->table('followers', ['engine'=>'InnoDB', 'signed'=>false]);
+        $table->addColumn('user_id',     'integer', ['comment' => '用户id'])
+              ->addColumn('follower_id', 'integer', ['comment' => '粉丝id'])
+              ->addTimestamps()
+              ->addIndex('user_id')
+              ->addIndex('follower_id')
+              ->create();
+    }
+}
