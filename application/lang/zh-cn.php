436ca2f137cf108e2c1b5b3586053760897d09e9
doderick <doderick@outlook.com>
2020-01-13 20:56:35 +0800

    多语言配置

---
+++ application/lang/zh-cn.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * @Author: doderick
+ * @Date: 2020-01-12 15:34:01
+ * @LastEditTime : 2020-01-13 20:45:06
+ * @LastEditors  : doderick
+ * @Description:
+ * @FilePath: /tp5/application/lang/zh-cn.php
+ */
+
+return [
+    'Name'                     => '用户名',
+    'E-Mail Address'           => '邮箱地址',
+    'Password'                 => '密码',
+    'Confirm Password'         => '确认密码',
+    'Forgot Your Password?'    => '忘记密码？',
+    'Remember Me'              => '请记住我',
+    'Captcha'                  => '验证码',
+    'Login'                    => '登录',
+    'Register'                 => '注册',
+    'Reset Password'           => '重置密码',
+    'Send Password Reset Link' => '发送密码重置链接',
+    'Register Now!'            => '现在注册！',
+    'New to doderick?'         => '还没账号？',
+];
